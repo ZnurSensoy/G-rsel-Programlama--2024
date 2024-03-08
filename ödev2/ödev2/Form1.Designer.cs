122b43ee22e4c996a6a96e9f263456c762057f71
Nur Şensoy <zahidenursensoy@gmail.com>
2024-03-08 21:40:43 +0300

    Hafta 2

---
+++ ödev2/ödev2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ödev2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(280, 219);
+            button1.Name = "button1";
+            button1.Size = new Size(190, 51);
+            button1.TabIndex = 0;
+            button1.Text = "ÇALIŞTIR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.Location = new Point(190, 292);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Mesajın var!";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Window;
+            textBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            textBox1.Location = new Point(177, 140);
+            textBox1.MaximumSize = new Size(5, 8);
+            textBox1.MinimumSize = new Size(400, 60);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(400, 60);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label2.ForeColor = SystemColors.InactiveCaptionText;
+            label2.Location = new Point(205, 413);
+            label2.Name = "label2";
+            label2.Size = new Size(362, 28);
+            label2.TabIndex = 3;
+            label2.Text = "22410082028- ZAHİDE NUR ŞENSOY";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Zahide Nur Şensoy -22410082028 ödev2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+    }
+}
---
+++ ödev2/ödev2/Form1.cs
@@ -0,0 +1,16 @@
+namespace ödev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text + "\n Görsel Programlamaya Hoşgeldin! \n Bu Dönem Çok Fazla Ödev Yapacaksın :)";
+
+        }
+    }
+}
